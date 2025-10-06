121d4d86161f9900cb69149af633ac9523157a37
Andrey Katunin <katunin4@yandex.ru>
2025-10-06 14:29:17 +0000

    test button

--- src/pages/LinksTable.tsx
+++ src/pages/LinksTable.tsx
@@ -1369,7 +1369,7 @@ const LinksTable = () => {
               100 ссылок: 21 000 рублей (-40%)
             </p>
           </div>
-          <button className="w-full bg-gradient-to-r from-primary to-secondary hover:from-secondary hover:to-primary transition-colors text-white font-medium py-2.5 rounded-lg shadow-md">
+          <button className="w-full bg-gradient-to-r from-primary to-secondary hover:from-secondary hover:to-primary transition-colors text-white font-medium py-2.5 rounded-lg shadow-md flex items-center justify-center">
             Перейти к оплате
           </button>
           <p className="text-gray-600 mb-4">
