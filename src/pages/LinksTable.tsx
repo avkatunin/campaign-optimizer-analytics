805d11d2306da8c4ee096c71672021b39935c4bb
Andrey Katunin <katunin4@yandex.ru>
2025-10-06 10:45:01 +0000

    fix new fix

--- src/pages/LinksTable.tsx
+++ src/pages/LinksTable.tsx
@@ -1369,10 +1369,7 @@ const LinksTable = () => {
               100 ссылок: 21 000 рублей (-40%)
             </p>
           </div>
-          <button
-            style="text-align: center"
-            className="w-full bg-gradient-to-r from-primary to-secondary hover:from-secondary hover:to-primary transition-colors text-white font-medium py-2.5 rounded-lg shadow-md items-center"
-          >
+          <button className="w-full bg-gradient-to-r from-primary to-secondary hover:from-secondary hover:to-primary transition-colors text-white font-medium py-2.5 rounded-lg shadow-md">
             Перейти к оплате
           </button>
           <p className="text-gray-600 mb-4">
