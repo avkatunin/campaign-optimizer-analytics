2c9ef0721abfada4da47e4fd3433749e1b29351a
Andrey Katunin <katunin4@yandex.ru>
2025-10-22 17:42:20 +0000

    fix logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,15 @@
-
 import { BookOpen } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Logo = () => {
   return (
-    <Link to="/" className="font-display text-xl font-bold flex items-center gap-2">
-      <div className="flex items-center justify-center bg-primary p-1.5 rounded-md text-white">
-        <BookOpen className="w-5 h-5" />
+    <Link
+      to="/"
+      className="font-display text-xl font-bold flex items-center gap-2"
+    >
+      <div className="text-2xl font-bold bg-gradient-primary bg-clip-text text-transparent">
+        ВнешкаПРО
       </div>
-      <span>Внешка<span className="text-primary">ПРО</span></span>
     </Link>
   );
 };
